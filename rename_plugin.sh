9a64567f6470080b67c4bcd2c9ca21ec946d9653
Chozabu <chozabu@gmail.com>
2015-08-18 18:14:17 +0100

    fix rename script

--- rename_plugin.sh
+++ rename_plugin.sh
@@ -5,4 +5,4 @@ echo "$newname"
 echo "$oldname"
 find . -not -path '*/\.*' -type f -print0 | xargs -0 sed -i "s/$oldname/$newname/g"
 #find . -type f -exec rename "s/$oldname/$newname/' '{}" \;
-#find . | sed -e "p;s/$oldname/$newname/" | xargs -n2 git mv
+find . | sed -e "p;s/$oldname/$newname/" | xargs -n2 git mv
