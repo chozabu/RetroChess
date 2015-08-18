c60a700078c2fe1641b0708698672e05044f2a10
Chozabu <chozabu@gmail.com>
2015-08-18 18:11:43 +0100

    added basic script to rename plugin

---
+++ rename_plugin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+newname=$1
+oldname="NetExample"
+echo "$newname"
+echo "$oldname"
+find . -not -path '*/\.*' -type f -print0 | xargs -0 sed -i "s/$oldname/$newname/g"
+#find . -type f -exec rename "s/$oldname/$newname/' '{}" \;
+#find . | sed -e "p;s/$oldname/$newname/" | xargs -n2 git mv
